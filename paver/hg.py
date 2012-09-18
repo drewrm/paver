2420a52908995def91908eb5800868ab34f0b0e4
Andrew Myers <andrew.myers1984@gmail.com>
2012-09-19 00:20:11 +1000

    Initial spike adding hg support

---
+++ paver/hg.py
@@ -0,0 +1,44 @@
+""" Convenience functions for working with mercurial. 
+
+Requires the hg binary on the path"""
+
+from paver.easy import sh
+
+def clone(url, dest):
+    """ Clones a mercurial repository """
+    sh("hg clone %(url)s %(dest)s" % dict(url=url, dest=dest))
+
+def pull(dest):
+    """ Performs a hg pull on the specified repository """
+    sh("cd %(dest)s; hg pull" % dict(dest=dest))
+
+def update(dest, rev="tip"):
+    sh("cd %(dest)s; hg update -r %(rev)s" % dict(dest=dest,rev=rev))
+
+def branches(dest, include_closed=False, __override__=None):
+    """ Get a list of branches as well as the current branch """
+    closed_flag = ""
+    if include_closed == True:
+        closed_flag = " -c"
+
+    if __override__ == None:
+        output = sh("cd %(dest)s; hg branches %(closed_flag)s" % dict(dest=dest, closed_flag=closed_flag))
+    else:
+        output = __override__
+
+    if output == None:
+        return None, []
+
+    branches = []
+    for line in output.split("\n"):
+        branch = line.strip().split()[0]
+        branches.append(branch)
+
+    output = None
+    output = sh("cd %(dest)s; hg branch" % dict(dest=dest))
+    if output == None:
+        current_branch = "default"
+    else:
+        current_branch = output.strip().split()[0]
+
+    return current_branch, branches
---
+++ paver/tests/test_hg.py
@@ -0,0 +1,45 @@
+from paver import hg, easy
+from paver.tests.mock import Mock, patch
+
+import os
+
+@patch(hg, "sh")
+def test_clone(sh):
+    hg.clone("https://foo", "bar")
+    assert sh.called
+    assert sh.call_args[0][0] == "hg clone https://foo bar"
+
+@patch(hg, "sh")
+def test_pull(sh):
+    hg.pull("bar")
+    assert sh.called
+    assert sh.call_args[0][0] == "cd bar; hg pull"
+
+@patch(hg, "sh")
+def test_simple_update(sh):
+    hg.update("bar")
+    assert sh.called
+    assert sh.call_args[0][0] == "cd bar; hg update -r tip"
+
+@patch(hg, "sh")
+def test_update_with_revision(sh):
+    hg.update("bar", "120")
+    assert sh.called
+    assert sh.call_args[0][0] == "cd bar; hg update -r 120"
+
+@patch(hg, "sh")
+def test_branches(sh):
+    branches = hg.branches("bar",
+            include_closed=False,
+            __override__="""default 7:bb638753724c
+        branch1 3:abb43356434c""")
+    assert branches == ("default", ["default", "branch1"])
+
+@patch(hg, "sh")
+def test_branches_with_closed(sh):
+    branches = hg.branches("bar",
+            include_closed=True,
+            __override__="""default 7:bb638753724c
+        branch1 3:abb43356434c
+        branch2 6:cbb63234214e (closed)""")
+    assert branches == ("default", ["default", "branch1", "branch2"])
